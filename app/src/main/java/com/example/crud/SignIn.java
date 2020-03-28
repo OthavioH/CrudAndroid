fee843d02ca4a3d26376b08f0394a48a09927705
OthavioH <othavio2650@gmail.com>
2020-03-28 02:27:12 -0300

    First commit

---
+++ app/src/main/java/com/example/crud/SignIn.java
@@ -0,0 +1,35 @@
+package com.example.crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bar,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.btnlogout:
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                overridePendingTransition(R.anim.fade_out,R.anim.fade_in);
+                startActivity(i);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/crud/Usuario.java
@@ -0,0 +1,62 @@
+package com.example.crud;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+    private Integer Id;
+    private String nome,cpf,email,telefone,senha;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+    @Override
+    public String toString(){
+        return nome;
+    }
+}
